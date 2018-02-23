cb6c9276e8382e38999a2e1f6a9e52a0ab38332f
Jaehee Kim <jaeheekim@Jaehees-MacBook-Pro.local>
2018-02-23 11:37:51 -0500

    changing code for opening the light box using function, adding more photos

--- instagram_clone.js
+++ instagram_clone.js
@@ -5,45 +5,35 @@ var imgArray = [
     im: 'Photos/1.jpg'
     },
     {
-    im: 'Photos/3.jpg'
+    im:'Photos/3.jpg'
     },
     { 
     im: 'Photos/2.jpg'
     },
     {
-    im: 'Photos/4.jpg'
+    im:'Photos/7.jpg'
     }, 
     {
-    im: 'Photos/5.jpg'
+    im:'Photos/9.jpg'
     },
     {
     im: 'Photos/6.jpg'
     },
     {
-    im:'Photos/7.jpg'
+    im:'Photos/4.jpg'
     },
     {
     im:'Photos/8.jpg'
+    },
+    {
+    im:'Photos/5.jpg'
     }
 ]
 //Images of the Gallery
 var imgContainer =  document.querySelector('.body-container');
 var lightBoxContainer = document.querySelector('.light-box');
-imgArray.forEach(function (picture, index) {
-    var imgElement = document.createElement('img');
-    imgElement.src = picture.im;
-    var source = imgElement.src;
-    imgContainer.appendChild(imgElement);
-    if (index === 1){
-        var lightBoxElement = document.createElement('img'); 
-        lightBoxElement.src = source;
-        lightBoxContainer.appendChild(lightBoxElement);
-    }
-});
 
-var featuredImage = document.querySelector('.light-box img');
-var allImages = document.querySelectorAll('.body-container img');
-
+//function for opening and closing light box.
 var openLightBox = function() {
     lightBoxContainer.classList.add('active');
 }
@@ -51,33 +41,23 @@ var closeLightBox = function(){
     lightBoxContainer.classList.remove('active');
 }
 
-//open light box
-allImages.forEach(function(nav){
-    nav.addEventListener('click', function(event){
-        event.preventDefault();
-        var source = nav.src;
-        featuredImage.src = source;
+//Click the image to open the lightbox
+imgArray.forEach(function (picture) {
+    var imgElement = document.createElement('img');
+    imgElement.src = picture.im;
+    var source = imgElement.src;
+    imgContainer.appendChild(imgElement);
+
+    imgElement.addEventListener('click', function(event){
+        var lightBoxElement = document.querySelector('.light-box-image')
+        lightBoxElement.src = source;
         openLightBox();
-    }
-  );
+    })
 });
-//close light box
+
+//closing the light box
 var closeButton = document.querySelector('.close-button');
 closeButton.addEventListener('click', function(event) {
     event.preventDefault();
     closeLightBox();
 });
-
-
-
-
-// Method 1: Simple for-loop
-// for(var i = 0; i < imgArray.length; i++) {
-//     var picture = imgArray[i];
-//     var imgElement = document.createElement('img');
-//     imgElement.src = picture.link;
-//     imgContainer.appendChild(imgElement);
-//   }
-
-
-
